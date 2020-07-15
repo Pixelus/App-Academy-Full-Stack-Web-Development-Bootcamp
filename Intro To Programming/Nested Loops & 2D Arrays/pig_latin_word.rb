0ac756714add05667e6c9ce4babef3433d25b977
Pixelus <s.malafosse@gmail.com>
2020-07-15 16:32:45 +0200

    Update pig_latin_word.rb

--- Intro To Programming/Nested Loops & 2D Arrays/pig_latin_word.rb
+++ Intro To Programming/Nested Loops & 2D Arrays/pig_latin_word.rb
@@ -10,50 +10,16 @@ def pig_latin_word(word)
   vowels = "aeiou"
   
   word.each_char.with_index do |char, i|
-    if i == 0
-      if vowels.include?(char)
-        new_word += word + "yay"
-      else
-          new_word = word.reverse() + "ay"
-        end
-      end
+    if vowels.include?(word[0])
+      new_word += word + "yay"
+      return new_word
+    elsif vowels.include?(char)
+      new_word = word[i..-1] + new_word + "ay"
+      return new_word
+    else
+      new_word << char
     end
   end
-  
-  return new_word
-end
-
-def transformed_word(word)
-  # new_word = ""
-  # vowels = "aeiou"
-
-  # test = word.split("")
-  # test.each do |word|
-  #   if !(vowels.include?(char))
-  #     new_word = test.shift() + new_word
-  #   end
-  # end
-  # return new_word
-  # chars = word.split("")
-  # transformed_word = ""
-  
-  # chars.each do |char|
-  #   #if i == 0 
-  #     if !(vowels.include?(char))
-  #       new_char = word.shift()
-  #       puts new_char
-  #     end
-  #   #end
-  # end
-  # i = 0
-  #       while i < word.length
-  #         if !(vowels.include?(char))
-  #           puts "e"
-  #         end
-  #         i += 1
-  #       end
-  #       #(word.split("")).shift()
-  #       new_word += word + "ay"
 end
 
 puts pig_latin_word("apple")   # => "appleyay"
