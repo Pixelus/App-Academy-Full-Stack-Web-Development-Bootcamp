a1a06b2eaa0f132b7d543568132560abc7f49067
Pixelus <s.malafosse@gmail.com>
2020-07-16 17:44:26 +0200

    Create last_index.rb

---
+++ Intro To Programming/Advanced Problems/last_index.rb
@@ -0,0 +1,18 @@
+# Write a method last_index that takes in a string and a character. The method should return the last index where the character can be found in the string.
+
+def last_index(str, char)
+  index_count = 0
+  i = 0
+  while i < str.length
+    if str[i] == char
+      index_count = i
+    end
+    i += 1
+  end
+  return index_count
+end
+
+puts last_index("abca", "a")       #=> 3
+puts last_index("mississipi", "i") #=> 9
+puts last_index("octagon", "o")    #=> 5
+puts last_index("programming", "m")#=> 7
