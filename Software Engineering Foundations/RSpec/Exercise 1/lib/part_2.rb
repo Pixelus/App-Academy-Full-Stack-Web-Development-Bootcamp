d1002b6f49b902f5c418dbd3548bb54c03ae7cde
pixelus <s.malafosse@gmail.com>
2020-07-24 16:14:17 +0200

    Update part_2.rb

--- Software Engineering Foundations/RSpec/Exercise 1/lib/part_2.rb
+++ Software Engineering Foundations/RSpec/Exercise 1/lib/part_2.rb
@@ -24,24 +24,22 @@ def vowel_counts(str)
 end
 
 def caesar_cipher(message, n)
-    shifted_str = ""
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    message.split(" ").each do |word|
-        word.each_char do |char|
-            if alphabet.include?(char)
-                shifted_str << alphabet[n] % 26
-            else
-                shifted_str << char
-            end
+    shifted_message = ""
+    alphabet = ("a".."z").to_a
+    message.each_char do |char|
+        old_index = alphabet.index(char)
+        new_index = old_index.to_i + n
+        if alphabet.include?(char)
+            shifted_message << alphabet[new_index % 26]
+        else
+            shifted_message << char
         end
     end
-    return shifted_str
+    shifted_message
 end
 
-puts vowel_counts("code bootcamp")
-puts vowel_counts("williamsburg bridge")
-puts vowel_counts("WILLIAMSBURG bridge")
-
-# puts caesar_cipher("123 _-!?@%", 3) # "123 _-!?@%"
-# puts caesar_cipher("welcome to week 3, day 1", 2)
-# puts caesar_cipher("hello world!", 1) # "ifmmp xpsme!"
+puts caesar_cipher("apple", 2) # bqqmf
+puts caesar_cipher("whoa", 52) # whoa
+puts caesar_cipher("123 _-!?@%", 3) # "123 _-!?@%"
+puts caesar_cipher("welcome to week 3, day 1", 2) #ygneqog vq yggm 3, fca 1
+puts caesar_cipher("hello world!", 1) # "ifmmp xpsme!"
