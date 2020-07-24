4dd77580b1b9d22f1979ea6457e9849152216a0c
pixelus <s.malafosse@gmail.com>
2020-07-24 17:20:02 +0200

    Update part_2.rb

--- Software Engineering Foundations/RSpec/Exercise 2/lib/part_2.rb
+++ Software Engineering Foundations/RSpec/Exercise 2/lib/part_2.rb
@@ -1,5 +1,18 @@
 def palindrome?(str)
-
+    new_str = []
+    word = str.split("")
+    puts str
+    puts word.join
+    word.each do |ele|
+        puts ele
+        puts word.pop
+        #new_str << word.pop
+    end
+    # puts new_str
+    # if new_str == str
+    #     return true
+    # end
+    false
 end
 
 def substrings(str)
@@ -9,3 +22,5 @@ end
 def palindrome_substrings(str)
 
 end
+
+puts palindrome?("racecar")
