0fe931b7e90098319a4c17802d4c89aa63aa7fd8
pixelus <s.malafosse@gmail.com>
2020-07-29 11:35:20 +0200

    Update part_2.rb

--- Software Engineering Foundations/RSpec/Exercise 2/lib/part_2.rb
+++ Software Engineering Foundations/RSpec/Exercise 2/lib/part_2.rb
@@ -1,26 +1,29 @@
 def palindrome?(str)
-    new_str = []
-    word = str.split("")
-    puts str
-    puts word.join
-    word.each do |ele|
-        puts ele
-        puts word.pop
-        #new_str << word.pop
+    new_str = ""
+    str.each_char.with_index do |char, i|
+        if str[i] == str[-i - 1]
+            new_str << char
+        end
     end
-    # puts new_str
-    # if new_str == str
-    #     return true
-    # end
-    false
+    if new_str == str
+        return true
+    else
+        return false
+    end
 end
 
 def substrings(str)
-
+    arr = []
+    (0...str.length).each do |start_index|
+        (0...str.length).each do |end_index|
+            if str[start_index..end_index] != ""
+                arr << str[start_index..end_index]
+            end
+        end
+    end
+    arr
 end
 
 def palindrome_substrings(str)
-
+    substrings(str).select { |substring| palindrome?(substring) == true && substring.length > 1 }
 end
-
-puts palindrome?("racecar")
