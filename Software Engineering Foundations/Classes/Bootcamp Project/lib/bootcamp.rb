2fea56c9d9365d937e88d466df1ec4d9ff4aa1ac
pixelus <s.malafosse@gmail.com>
2020-08-17 14:41:42 +0200

    Add files

---
+++ Software Engineering Foundations/Classes/Bootcamp Project/lib/bootcamp.rb
@@ -0,0 +1,69 @@
+class Bootcamp
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |hash, k| hash[k] = [] }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        teachers << teacher
+    end
+
+    def enroll(student)
+        if students.length < @student_capacity
+            @students << student
+            return true
+        end
+        false
+    end
+
+    def enrolled?(student)
+        if students.include?(student)
+            return true
+        end
+        false
+    end 
+
+    def student_to_teacher_ratio
+        students.length / teachers.length
+    end
+
+    def add_grade(student, grade)
+        if self.enrolled?(student) == true
+            @grades[student] << grade
+            return true
+        end
+        false
+    end
+
+    def num_grades(student)
+        @grades[student].length
+    end
+
+    def average_grade(student)
+        if self.enrolled?(student) == true && @grades[student].length != 0 
+            @grades[student].sum / @grades[student].length
+        else 
+            return nil
+        end
+    end
+end
