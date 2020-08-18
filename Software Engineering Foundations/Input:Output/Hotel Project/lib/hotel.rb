6683e78abf1aba4979639cf3683fe085ab3494f6
pixelus <s.malafosse@gmail.com>
2020-08-18 17:45:20 +0200

    Add files

---
+++ Software Engineering Foundations/Input:Output/Hotel Project/lib/hotel.rb
@@ -0,0 +1,46 @@
+require_relative "room"
+
+class Hotel
+    def initialize(name, capacities)
+        @name = name
+        @rooms = {} 
+        capacities.each do |room_name, capacity| 
+            @rooms[room_name] = Room.new(capacity)
+        end
+    end
+
+    def name
+        @name.split(" ").map(&:capitalize).join(" ")
+    end
+
+    def rooms
+        @rooms
+    end
+
+    def room_exists?(room_name)
+        if @rooms.has_key?(room_name)
+            return true
+        end
+        false
+    end
+
+    def check_in(person, room_name)
+        if !self.room_exists?(room_name)
+            puts "sorry, room does not exist"
+        elsif @rooms[room_name].add_occupant(person)
+            puts "check in successful"
+        else
+            puts "sorry, room is full"
+        end
+    end
+
+    def has_vacancy?
+        @rooms.values.any? { |room| room.available_space > 0 }
+    end
+
+    def list_rooms
+        @rooms.each do |room_name, room| 
+            print "#{room_name}.*#{room.available_space}\n"
+        end
+    end
+end
