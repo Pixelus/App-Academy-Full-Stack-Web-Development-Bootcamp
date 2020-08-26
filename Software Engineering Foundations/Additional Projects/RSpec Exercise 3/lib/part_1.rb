2f8a93ba29b729fe99381f0fb6aa7d3733b44d9e
pixelus <s.malafosse@gmail.com>
2020-08-26 17:42:19 +0200

    Add files

---
+++ Software Engineering Foundations/Additional Projects/RSpec Exercise 3/lib/part_1.rb
@@ -0,0 +1,26 @@
+def is_prime?(num)
+    factors = 0
+    (1..num).each { |factor| factors += 1 if num % factor == 0 }
+    return true if factors == 2
+    false
+end
+
+def nth_prime(n)
+    primes = []
+    number = 1
+    while primes.length < n
+        number += 1
+        primes << number if is_prime?(number)
+    end
+    number
+end
+
+p nth_prime(1) #2
+p nth_prime(2) #3
+p nth_prime(13) #7
+
+def prime_range(min, max)
+    prime_numbers = []
+    (min..max).each { |num| prime_numbers << num if is_prime?(num) }
+    prime_numbers
+end
---
+++ Software Engineering Foundations/Additional Projects/RSpec Exercise 3/lib/part_2.rb
@@ -0,0 +1,15 @@
+def element_count(arr)
+    count = Hash.new(0)
+    arr.each { |ele| count[ele] += 1 }
+    count
+end
+
+def char_replace!(str, hash)
+    str.each_char.with_index do |char, index|
+        str[index] = hash[char] if hash.has_key?(char)
+    end 
+end
+
+def product_inject(arr)
+    arr.inject { |product, n| product * n }
+end
